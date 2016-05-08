57ecabc2acd3317674974fd3f709be620b8cdb25
Seth DeSantis <desn117@mail.chapman.edu>
2016-05-08 11:51:11 -0700

    Clear's history, and current tab close setting is working

--- popup.js
+++ popup.js
@@ -2,52 +2,102 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
  
+ document.addEventListener('DOMContentLoaded', function () {
+      document.querySelector('#closeBrowser').addEventListener('change', CheckClosedSetting);
+});
+document.addEventListener('DOMContentLoaded', function () {
+      document.querySelector('#closeTab').addEventListener('change', CheckCloseTab);
+});
+document.addEventListener('DOMContentLoaded', function () {
+      document.querySelector('#useHotkey').addEventListener('change', CheckUseHotkey);
+});
+document.addEventListener('DOMContentLoaded', function () {
+      document.querySelector('#myBtn').addEventListener('click', browsingdata);
+});
+
+var closeBrow = false;
+var closeCurrTab = false;
+var useHotAkey = false;
+var closeCurrentTab = { active: true, currentWindow: true };
+
+ function CloseActiveTab(tabs) {
+  var currentTab = tabs[0];
+  console.log(currentTab);
+  chrome.tabs.remove(tabs[0].id,function() {
+       console.log("Removing Active Tab");
+  }
+    );
+ }
+
+ 
+function CheckClosedSetting(){ 
+   if(closeBrowser.checked){
+    console.log("I am checked [Close Browser]");
+    closeBrow =true;
+   }
+   else{
+     closeBrow = false;
+   }
+}
+function CheckUseHotkey(){
+   if(useHotkey.checked){
+     useHotAkey = true;
+    console.log("I am checked [Use Hotkey]");
+   }
+   else{
+     useHotAkey= false;
+      //do something else
+   }
+}
+
+function CheckCloseTab(){
+   if(closeTab.checked){
+    console.log("I am checked [Close Tab]");
+   closeCurrTab=true;
+   }
+   else{
+     closeCurrTab = false;
+   }
+}
+
+
+
+
+function browsingdata(){
+    chrome.browsingData.remove({
+      "originTypes": {
+        "protectedWeb": true,  
+        "unprotectedWeb":true,
+        "extension":true   
+      }
+    }, {
+      "appcache": true,
+      "cache": true, 
+      "cookies": true, 
+      "downloads": true,
+      "fileSystems": true,
+      "formData": true,
+      "history": true, 
+      "indexedDB": true, 
+      "localStorage": true,
+      "pluginData": true, 
+      "passwords": true, 
+      "webSQL": true 
+    }, function (){
+        console.log("All data is Deleted...");
+    });
+   
+   
+   if(closeBrow==true){
+     // Call function to close browser
+     console.log('closing browser');
+   }  
+   else if(closeCurrTab==true){
+     // Close current tab
+     console.log('closing current tab'); 
+     chrome.tabs.query(closeCurrentTab, CloseActiveTab);
+   }
+}
+
+  
