6eda59aaff1ef0cafd711e11468c85f3853b6c8a
Seth DeSantis <desn117@mail.chapman.edu>
2016-05-07 21:05:29 -0700

    Skelton uploaded

---
+++ settings.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+      document.querySelector('#closeBrowser').addEventListener('change', CheckClosedSetting);
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+      document.querySelector('#useHotkey').addEventListener('change', CheckUseHotkey);
+});
+
+function CheckClosedSetting(){
+   //Do Something...maybe another function showAlert(), for instance
+   if(closeBrowser.checked){
+    console.log("I am checked [Close Browser]");
+   }
+   else{
+      //do something else
+   }
+}
+
+function CheckUseHotkey(){
+   if(useHotkey.checked){
+    console.log("I am checked [Use Hotkey]");
+   }
+   else{
+      //do something else
+   }
+}
+
+ 
